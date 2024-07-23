7e7e6b9bc541192c80c70218dabb4b8913aeeebc
Anand-sk1324 <anand.sk1324@gmail.com>
2024-07-24 01:02:26 +0530

    v1.22

--- src/components/SubHeading.tsx
+++ src/components/SubHeading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SubHeading = ({ children }) => {
   return (
-    <h3 className="text-base md:text-lg tracking-wider text-slate-400">
+    <h3 className="text-base md:text-lg text-slate-200 font-extrabold tracking-widest">
       {children}
     </h3>
   );
