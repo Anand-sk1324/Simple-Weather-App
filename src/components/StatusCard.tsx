a445aaf3a96eaf07106803c4c990d3b2281afa66
Anand-sk1324 <anand.sk1324@gmail.com>
2024-07-24 00:56:59 +0530

    v1.21

--- src/components/StatusCard.tsx
+++ src/components/StatusCard.tsx
@@ -5,7 +5,9 @@ const StatusCard = ({ Icon, title, value }) => {
     <div className="">
       <div className="grid grid-rows-[auto_auto] grid-cols-[40px_auto] place-items-center gap-1 ">
         {Icon}
-        <p className="place-self-start text-lg lg:text-xl">{title}</p>
+        <p className="place-self-start text-lg lg:text-xl text-nowrap">
+          {title}
+        </p>
         <p className=" col-start-2 place-self-start text-xl lg:text-2xl text-slate-200">
           {value}
         </p>
